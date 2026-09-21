2fb9f76eab2e04f0a53444385af92f360b886aaa
newstarwnn <3470699526@qq.com>
2026-09-21 17:38:48 +0800

    second commit

--- src/main/java/com/wnn/gittext.java
+++ src/main/java/com/wnn/gittext.java
@@ -3,5 +3,6 @@ package com.wnn;
 public class gittext {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello git2!");
     }
 }
